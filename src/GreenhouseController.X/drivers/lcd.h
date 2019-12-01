7d3a3f92c490ec081929b34c1f3667655b2d620d
Robert Bennett <robertbennett1998@outlook.com>
2019-12-01 13:04:22 +0000

    Added the Lcd code to the src directory.

--- src/GreenhouseController.X/drivers/lcd.h
+++ src/GreenhouseController.X/drivers/lcd.h
@@ -1,12 +1,14 @@
-/***********************************************************************
- * NAME: lcd.h 
- * PURPOSE: provide the api available for the lcd.
- * AUTHOR: R.Bennett
-***********************************************************************/
-#ifndef LCD
-#define LCD
+#ifndef LCD_H
+#define	LCD_H
 
+#include "std.h"
 
-
+void Lcd_Init(void);
+inline void Lcd_SetDisplayMode(const bool bLcdOn, const bool bCursorOn, const bool bBlinkOn);
+void Lcd_SetCursorPosition(uchar ubyPos, uchar ubyLine);
+void Lcd_WriteCharacter(const char c);
+void Lcd_WriteString(const char* pStr);
+void Lcd_RecallLastPosition();
+inline void Lcd_ClearDisplay();
 
 #endif
